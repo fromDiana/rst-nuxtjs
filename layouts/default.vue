1abcae2b86d608ca2ec729b9d15acbed105ef9bd
diana <orsolyovadianka@gmail.com>
2024-03-05 21:00:47 +0100

    fixed font

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,6 +22,8 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 html,
 body {
     margin: 0;
@@ -33,9 +35,8 @@ body {
     scrollbar-width: none;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    /* font-family: 'Poppins', sans-serif !important; */
-    font-family: 'Poppins', sans-serif !important;
-    src: url('~assets/fonts/Poppins/Poppins-Regular.ttf') format('truetype');
+    font-family: "Poppins", sans-serif !important;
+    font-style: normal;
     font-style: normal;
     color: white;
     -webkit-user-select: none;
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,19 +4,22 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'rst',
+    title: 'RST',
     htmlAttrs: {
       lang: 'en'
     },
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
