912674c1ae4bdcba18816098ccc834815c2c08a1
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:30:44 +0100

    added alttxt to post2

--- assets/posts/post3/data.js
+++ assets/posts/post3/data.js
@@ -3,11 +3,27 @@ export default {
       title: "Vylepšenie WACOM tabletu",
       date: "2023-12-04",
       description: "Prečistený, prepastovaný a upgradnutý na 32GB ram + 1TB SSD",
+      images: [
+          { name: "image1.jpg", alt: "rozobratie wacom tabletu kosice" },
+          { name: "image2.jpg", alt: "cistenie wacom chladenia" },
+          { name: "image3.jpg", alt: "prepastovanie wacom tabletu kosice" },
+          { name: "image4.jpg", alt: "rozsirenie ram wacom" },
+          { name: "image5.jpg", alt: "cistenie chladenia kosice" },
+          { name: "image6.jpg", alt: "repasovany wacom kosice" },
+      ]
     },
     en: {
       title: "WACOM tablet upgrade",
       date: "2023-12-04",
       description: "Cleaned, repasted, and upgraded to 32GB RAM + 1TB SSD",
+      images: [
+          { name: "image1.jpg", alt: "disassembly of wacom tablet kosice" },
+          { name: "image2.jpg", alt: "cleaning wacom cooling" },
+          { name: "image3.jpg", alt: "overheating wacom tablet kosice" },
+          { name: "image4.jpg", alt: "ram upgrade wacom tablet" },
+          { name: "image5.jpg", alt: "Cooling cleaning kosice" },
+          { name: "image6.jpg", alt: "renewed wacom kosice" },
+      ]
     }
   };
   
