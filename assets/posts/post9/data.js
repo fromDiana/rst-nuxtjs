f629d82bd52f3318e8efb8ff2eab448e08030ae2
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:45:18 +0100

    added alttxt to post90

--- assets/posts/post9/data.js
+++ assets/posts/post9/data.js
@@ -3,11 +3,23 @@ export default {
       title: "Apple plný prachu",
       date: "2022-05-10",
       description: "Aj o MacBook sa sem tam treba postarať. Najmä ak používate svoj notebook v posteli alebo na kolenách a podobne.",
+      images: [
+          { name: "image1.jpg", alt: "Cistenie apple notebooku kosice" },
+          { name: "image2.jpg", alt: "cistenie macbooku kosice" },
+          { name: "image3.jpg", alt: "prehrievanie macbooku kosice" },
+          { name: "image4.jpg", alt: "prehrievanie apple kosice" },
+      ]
     },
     en: {
       title: "Dusty MacBook",
       date: "2022-05-10",
       description: "Even a MacBook needs some care from time to time. Especially if you use your laptop in bed or on your lap, and so on.",
+      images: [
+          { name: "image1.jpg", alt: "Apple laptop cleaning kosice" },
+          { name: "image2.jpg", alt: "MacBook cleaning kosice" },
+          { name: "image3.jpg", alt: "overheating MacBook kosice" },
+          { name: "image4.jpg", alt: "overheating Apple kosice" },
+      ]
     }
   };
   
