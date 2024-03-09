5c631095883ee7edef19354df4389c800be1a6ea
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:08:39 +0100

    added post10 alt text

--- assets/posts/post10/data.js
+++ assets/posts/post10/data.js
@@ -3,11 +3,21 @@ export default {
       title: "Malinké PC vhodné na kancelársku prácu",
       date: "2022-05-05",
       description: "Tento malý PC bol vyslovene zostavený na tichú a úspornú prevádzku pre kancelárske využitie. Banana for scale.",
+      images: [
+          { name: "image1.jpg", alt: "Mala zostava na mieru kosice" },
+          { name: "image2.jpg", alt: "skladanie pc kosice" },
+          { name: "image3.jpg", alt: "banana for scale" },
+      ]
     },
     en: {
       title: "Small PC suitable for office work",
       date: "2022-05-05",
       description: "This small PC was specifically assembled for quiet and efficient operation for office use. Banana for scale.",
+      images: [
+          { name: "image1.jpg", alt: "Mini custom built pc kosice" },
+          { name: "image2.jpg", alt: "pc build kosice" },
+          { name: "image3.jpg", alt: "banana for scale" },
+      ]
     }
   };
   
