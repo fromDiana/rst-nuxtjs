5acc7a9b844bb76d5b89a659b9bfbdf855e76c01
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:38:22 +0100

    added alttxt to post6

--- assets/posts/post6/data.js
+++ assets/posts/post6/data.js
@@ -3,11 +3,21 @@ export default {
       title: "Vysušená teplovodivá pasta? Prach?",
       date: "2022-02-21",
       description: "Dodržiavanie intervalov čistenia a prepastovania pomôže vašemu počítaču udržať si výkon a zároveň, mu predlžujete obdobie spoľahlivého chodu.",
+      images: [
+          { name: "image1.jpg", alt: "procesor sucha teplovodiva pasta prehrievanie" },
+          { name: "image2.jpg", alt: "graficka karta sucha teplovodiva pasta prehrievanie" },
+          { name: "image3.jpg", alt: "prach v pocitaci prehrievanie" },
+      ]
     },
     en: {
       title: "Dried thermal paste? Dust?",
       date: "2022-02-21",
       description: "Adhering to cleaning and repasting intervals helps your computer maintain its performance and, at the same time, extends the period of reliable operation.",
+      images: [
+          { name: "image1.jpg", alt: "dry thermalpaste on processor overheating" },
+          { name: "image2.jpg", alt: "dry thermalpaste in graphics card overheating" },
+          { name: "image3.jpg", alt: "dust in a computer overheating" },
+      ]
     }
   };
   
