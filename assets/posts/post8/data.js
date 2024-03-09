5dd9106cafaf6a29f6a22fe5a62923974f25dc37
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:43:35 +0100

    added alttxt to post89

--- assets/posts/post8/data.js
+++ assets/posts/post8/data.js
@@ -3,11 +3,23 @@ export default {
       title: "Threadripper 3970X",
       date: "2021-08-30",
       description: "Threadripper 3970X od AMD nie je to najjednoduchšie na uchladenie ale s touto perfektnou skrinkou a slušnou nádielkou Noctua fanov sme to zvládli skoro nepočuteľne.",
+      images: [
+          { name: "image1.jpg", alt: "Pracovna stanica na mieru kosice" },
+          { name: "image2.jpg", alt: "Pracovna stanica kosice" },
+          { name: "image3.jpg", alt: "Ticha pracovna stanica kosice" },
+          { name: "image4.jpg", alt: "Spolahliva pracovna stanica kosice" },
+      ]
     },
     en: {
       title: "Threadripper 3970X",
       date: "2021-08-30",
       description: "The AMD Threadripper 3970X isn't the easiest to cool, but with this perfect case and a decent supply of Noctua fans, we managed to do it almost inaudibly.",
+      images: [
+          { name: "image1.jpg", alt: "Custom workstation kosice" },
+          { name: "image2.jpg", alt: "workstation kosice" },
+          { name: "image3.jpg", alt: "Silent workstation kosice" },
+          { name: "image4.jpg", alt: "reliable workstation kosice" },
+      ]
     }
   };
   
