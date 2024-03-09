96196d1576e5b6adb342366d69e91daaf47a63e5
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:36:12 +0100

    added alttxt to post5

--- assets/posts/post5/data.js
+++ assets/posts/post5/data.js
@@ -3,11 +3,23 @@ export default {
       title: "Biela a RGB, zas?",
       date: "2023-02-10",
       description: "Biela farba s RGB podsvietením sa zatiaľ neokukala. Nespornou výhodou je možnosť nastavenia podsvietenia v celom spektre.",
+      images: [
+          { name: "image1.jpg", alt: "RGB herny pocitac kosice" },
+          { name: "image2.jpg", alt: "podsvieteny herny pocitac" },
+          { name: "image3.jpg", alt: "custom kabelaz herny pocitac" },
+          { name: "image4.jpg", alt: "krasny herny pocitac" },
+      ]
     },
     en: {
       title: "White and RGB, again?",
       date: "2023-02-10",
       description: "The white color with RGB lighting hasn't gone out of fashion yet. The undeniable advantage is the ability to set the lighting across the entire spectrum.",
+      images: [
+          { name: "image1.jpg", alt: "RGB gaming computer" },
+          { name: "image2.jpg", alt: "LED gaming computer" },
+          { name: "image3.jpg", alt: "custom cable gaming computer" },
+          { name: "image4.jpg", alt: "beautiful gaming computer" },
+      ]
     }
   };
   
