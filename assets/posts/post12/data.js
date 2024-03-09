8a23234b57cb0bd84e7ac35ccd22fccaa3a7a31f
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:14:57 +0100

    added alttxt to post12

--- assets/posts/post12/data.js
+++ assets/posts/post12/data.js
@@ -3,11 +3,23 @@ export default {
       title: "Red and black",
       date: "2020-03-16",
       description: "Po náročnom a zdĺhavom testovaní sme overili 100% stabilitu pri 5Ghz na 9700k a skĺbili absolútne ticho pri štandardnom používaní s nízkymi teplotami pod absolútne maximálnym vyťažením ako procesora tak ramiek aj grafickej karty.",
+      images: [
+          { name: "image1.jpg", alt: "dizajnova pc zostava kosice" },
+          { name: "image2.jpg", alt: "dizajnova zostava na mieru kosice" },
+          { name: "image3.jpg", alt: "Graficka karta herna" },
+          { name: "image4.jpg", alt: "herna podsvietena zostava kosice" },
+      ]
     },
     en: {
       title: "Red and black",
       date: "2020-03-16",
       description: "After demanding and lengthy testing, we have confirmed 100% stability at 5GHz on the 9700k and achieved absolute silence during standard use with low temperatures under absolute maximum load for both the processor, RAM, and graphics card.",
+      images: [
+          { name: "image1.jpg", alt: "Design PC setup kosice" },
+          { name: "image2.jpg", alt: "Custom-designed PC setup kosice" },
+          { name: "image3.jpg", alt: "gaming gpu" },
+          { name: "image4.jpg", alt: "Gaming illuminated setup kosice" },
+      ]
     }
   };
   
