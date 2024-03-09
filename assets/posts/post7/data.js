b0cf753182f9762b9ad905f1d4c23b2b396affea
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:41:21 +0100

    added alttxt to post7

--- assets/posts/post7/data.js
+++ assets/posts/post7/data.js
@@ -3,11 +3,23 @@ export default {
       title: "Čierno-biela s RGB",
       date: "2022-02-21",
       description: "Veľmi výrazná a zaujimavá konfigurácia. Celé podsvietenie (RAM, GPU, Fany) je ovládané cez ASUS Aura Sync a teda si vie zákazník jedným klikom meniť farby na celom PC.",
+      images: [
+          { name: "image1.jpg", alt: "Fialovo biela herna zostava" },
+          { name: "image2.jpg", alt: "Herna zostava s podsvietenim biela fialova" },
+          { name: "image3.jpg", alt: "Herny pocitac s podsvietenim biela fialova" },
+          { name: "image4.jpg", alt: "Pocitac s podsvietenim a kablami na mieru" },
+      ]
     },
     en: {
       title: "Black and white with RGB",
       date: "2022-02-21",
       description: "A very distinctive and interesting configuration. The entire lighting (RAM, GPU, fans) is controlled through ASUS Aura Sync, allowing the customer to change colors across the entire PC with just one click.",
+      images: [
+          { name: "image1.jpg", alt: "White-purple gaming computer" },
+          { name: "image2.jpg", alt: "gaming computer with purple backlight" },
+          { name: "image3.jpg", alt: "custom built computer with purple backlight" },
+          { name: "image4.jpg", alt: "Custom build computer with backlight and custom cables" },
+      ]
     }
   };
   
