921142b84087757d03a269961bc23add23ee269e
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 00:32:50 +0100

    added alttxt to post4

--- assets/posts/post4/data.js
+++ assets/posts/post4/data.js
@@ -3,11 +3,23 @@ export default {
       title: "Intel i5, 16GB ram, RX6700XT",
       date: "2023-11-10",
       description: "Jednoduchšia herná zostava ktorá ale neurazí ani svojim vzhľadom ani výkonom.",
+      images: [
+          { name: "image1.jpg", alt: "RGB herna zostava kosice" },
+          { name: "image2.jpg", alt: "herny pocitac kosice" },
+          { name: "image3.jpg", alt: "tichy herny pocitac kosice" },
+          { name: "image4.jpg", alt: "tuning pocitac kosice" },
+      ]
     },
     en: {
       title: "White and RGB, again?",
       date: "2023-11-10",
       description: "A simpler gaming setup that won't disappoint in either looks or performance.",
+      images: [
+          { name: "image1.jpg", alt: "RGB gaming pc kosice" },
+          { name: "image2.jpg", alt: "gaming computer kosice" },
+          { name: "image3.jpg", alt: "silent gaming pc kosice" },
+          { name: "image4.jpg", alt: "tuning computer kosice" },
+      ]
     }
   };
   
