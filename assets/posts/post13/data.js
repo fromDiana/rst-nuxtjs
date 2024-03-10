7f013c60992d94eddf77f8a0e07ecffb7dd3072b
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 22:55:49 +0100

    post13 alttxt

--- assets/posts/post13/data.js
+++ assets/posts/post13/data.js
@@ -4,9 +4,9 @@ export default {
       date: "2020-02-14",
       description: "Dnes nás čakal hrkajúci DELL. Naštastie sa jednalo iba o ventilátor. No, bola potrebná 100% istota a teda sa šlo do komplexnej diagnostiky.",
       images: [
-          { name: "image1.jpg", alt: "prepastovanie notebooku kosice" },
-          { name: "image2.jpg", alt: "cistenie notebooku kosice" },
-          { name: "image3.jpg", alt: "prehrievanie notebooku kosice" },
+          { name: "image1.jpg", alt: "prepastovanie notebooku" },
+          { name: "image2.jpg", alt: "cistenie notebooku" },
+          { name: "image3.jpg", alt: "prehrievanie notebooku" },
           { name: "image4.jpg", alt: "prehrievanie DELL" },
       ]
     },
@@ -15,9 +15,9 @@ export default {
       date: "2020-02-14",
       description: "Today we encountered a noisy DELL. Fortunately, it was only the fan. However, 100% certainty was needed, so we proceeded with a comprehensive diagnosis.",
       images: [
-          { name: "image1.jpg", alt: "laptop re-paste kosice" },
-          { name: "image2.jpg", alt: "cleaning laptop kosice" },
-          { name: "image3.jpg", alt: "overheating laptop kosice" },
+          { name: "image1.jpg", alt: "laptop re-paste" },
+          { name: "image2.jpg", alt: "cleaning laptop" },
+          { name: "image3.jpg", alt: "overheating laptop" },
           { name: "image4.jpg", alt: "overheating DELL" },
       ]
     }
