2ec08410ca04747dab4d5a6dc5a9952641f2c736
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 22:53:23 +0100

    post1 alttxt

--- assets/posts/post1/data.js
+++ assets/posts/post1/data.js
@@ -5,8 +5,8 @@ export default {
       date: "2024-01-01",
       description: "Krásna PC zostava určená na prácu v 3D programoch.",
       images: [
-          { name: "image1.jpg", alt: "Pocitac na mieru kosice" },
-          { name: "image2.jpg", alt: "Zostava na mieru kosice" },
+          { name: "image1.jpg", alt: "PC Zostava na mieru" },
+          { name: "image2.jpg", alt: "PC na pracu" },
           { name: "image3.jpg", alt: "Graficka karta herna" },
           { name: "image4.jpg", alt: "Asus tuf" },
       ]
@@ -16,8 +16,8 @@ export default {
       date: "2024-01-01",
       description: "Gorgeous custom built PC used mostly 3D software work",
       images: [
-          { name: "image1.jpg", alt: "build pc kosice" },
-          { name: "image2.jpg", alt: "Custom built pc kosice" },
+          { name: "image1.jpg", alt: "custom pc" },
+          { name: "image2.jpg", alt: "Custom built pc" },
           { name: "image3.jpg", alt: "Gaming gpu" },
           { name: "image4.jpg", alt: "Asus tuf" },
       ]
