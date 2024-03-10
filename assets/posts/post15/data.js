4d6c7d601e4cc5f867fd3c1faa34e54dba2e2655
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 22:56:46 +0100

    post15 alttxt

--- assets/posts/post15/data.js
+++ assets/posts/post15/data.js
@@ -4,10 +4,10 @@ export default {
       date: "2020-02-10",
       description: "Prečistený, prepastovaný so zvýšením kapacity pamäte ram a vymenenie staršieho harddisku za nový ssd disk. Týmto zásahom notebook patrí znova ku krásne svižným.",
       images: [
-          { name: "image1.jpg", alt: "zrychlenie notebooku kosice" },
-          { name: "image2.jpg", alt: "precistenie notebooku kosice" },
-          { name: "image3.jpg", alt: "prepastovanie notebooku kosice" },
-          { name: "image4.jpg", alt: "repas notebooku kosice" },
+          { name: "image1.jpg", alt: "zrychlenie notebooku" },
+          { name: "image2.jpg", alt: "precistenie notebooku" },
+          { name: "image3.jpg", alt: "prepastovanie notebooku" },
+          { name: "image4.jpg", alt: "repas notebooku" },
           { name: "image5.jpg", alt: "vymena harddisku" },
           { name: "image6.jpg", alt: "rozsirenie ram" },
       ]
@@ -17,10 +17,10 @@ export default {
       date: "2020-02-10",
       description: "Cleaned, re-pasted with increased RAM capacity, and replaced the older hard disk with a new SSD. With these interventions, the notebook is once again beautifully swift.",
       images: [
-          { name: "image1.jpg", alt: "speed up laptop kosice" },
-          { name: "image2.jpg", alt: "laptop cleaning kosice" },
-          { name: "image3.jpg", alt: "laptop re-paste kosice" },
-          { name: "image4.jpg", alt: "laptop overheating kosice" },
+          { name: "image1.jpg", alt: "speed up laptop" },
+          { name: "image2.jpg", alt: "laptop cleaning" },
+          { name: "image3.jpg", alt: "laptop re-paste" },
+          { name: "image4.jpg", alt: "laptop overheating" },
           { name: "image5.jpg", alt: "harddrive upgrade" },
           { name: "image6.jpg", alt: "ram upgrade" },
       ]
