f8545bfa999e58054e01046495bb53a30d8e36b6
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 22:56:09 +0100

    post14 alttxt

--- assets/posts/post14/data.js
+++ assets/posts/post14/data.js
@@ -4,7 +4,7 @@ export default {
       date: "2020-02-30",
       description: "Tejto Nvidia GTX 1080Ti sa po zákroku teploty znížili o 10°c. Veríme že bude naďalej slúžiť ako sa patrí na plný výkon s príjemnými teplotami a tichým chodom.",
       images: [
-          { name: "image1.jpg", alt: "Prepastovanie grafickej karty kosice" },
+          { name: "image1.jpg", alt: "Prepastovanie grafickej karty" },
           { name: "image2.jpg", alt: "Prehrievanie grafickej karty" },
           { name: "image3.jpg", alt: "znizenie teplot grafickej karty" },
       ]
@@ -14,7 +14,7 @@ export default {
       date: "2020-02-30",
       description: "After this intervention, the temperatures of this Nvidia GTX 1080Ti decreased by 10°C. We believe it will continue to perform as expected, with pleasant temperatures and quiet operation.",
       images: [
-          { name: "image1.jpg", alt: "re-pasting graphics card kosice" },
+          { name: "image1.jpg", alt: "re-pasting graphics card" },
           { name: "image2.jpg", alt: "overheating graphics card" },
           { name: "image3.jpg", alt: "lowering temps graphics card" },
       ]
