e03305821a6ea2f182eccb6bc9c0eb51a80d75b2
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 22:58:12 +0100

    post3 alttxt

--- assets/posts/post3/data.js
+++ assets/posts/post3/data.js
@@ -4,12 +4,12 @@ export default {
       date: "2023-12-04",
       description: "Prečistený, prepastovaný a upgradnutý na 32GB ram + 1TB SSD",
       images: [
-          { name: "image1.jpg", alt: "rozobratie wacom tabletu kosice" },
+          { name: "image1.jpg", alt: "rozobratie wacom tabletu" },
           { name: "image2.jpg", alt: "cistenie wacom chladenia" },
-          { name: "image3.jpg", alt: "prepastovanie wacom tabletu kosice" },
+          { name: "image3.jpg", alt: "prepastovanie wacom tabletu" },
           { name: "image4.jpg", alt: "rozsirenie ram wacom" },
-          { name: "image5.jpg", alt: "cistenie chladenia kosice" },
-          { name: "image6.jpg", alt: "repasovany wacom kosice" },
+          { name: "image5.jpg", alt: "cistenie chladenia" },
+          { name: "image6.jpg", alt: "repasovany wacom" },
       ]
     },
     en: {
@@ -17,12 +17,12 @@ export default {
       date: "2023-12-04",
       description: "Cleaned, repasted, and upgraded to 32GB RAM + 1TB SSD",
       images: [
-          { name: "image1.jpg", alt: "disassembly of wacom tablet kosice" },
+          { name: "image1.jpg", alt: "disassembly of wacom tablet" },
           { name: "image2.jpg", alt: "cleaning wacom cooling" },
-          { name: "image3.jpg", alt: "overheating wacom tablet kosice" },
+          { name: "image3.jpg", alt: "overheating wacom tablet" },
           { name: "image4.jpg", alt: "ram upgrade wacom tablet" },
-          { name: "image5.jpg", alt: "Cooling cleaning kosice" },
-          { name: "image6.jpg", alt: "renewed wacom kosice" },
+          { name: "image5.jpg", alt: "Cooling cleaning" },
+          { name: "image6.jpg", alt: "renewed wacom" },
       ]
     }
   };
