1130f7c377790cf98454ee861e28c0cca667d4ab
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 23:00:41 +0100

    post9 alttxt

--- assets/posts/post9/data.js
+++ assets/posts/post9/data.js
@@ -4,10 +4,10 @@ export default {
       date: "2022-05-10",
       description: "Aj o MacBook sa sem tam treba postarať. Najmä ak používate svoj notebook v posteli alebo na kolenách a podobne.",
       images: [
-          { name: "image1.jpg", alt: "Cistenie apple notebooku kosice" },
-          { name: "image2.jpg", alt: "cistenie macbooku kosice" },
-          { name: "image3.jpg", alt: "prehrievanie macbooku kosice" },
-          { name: "image4.jpg", alt: "prehrievanie apple kosice" },
+          { name: "image1.jpg", alt: "Cistenie apple notebooku" },
+          { name: "image2.jpg", alt: "cistenie macbooku" },
+          { name: "image3.jpg", alt: "prehrievanie macbooku" },
+          { name: "image4.jpg", alt: "prehrievanie apple" },
       ]
     },
     en: {
@@ -15,10 +15,10 @@ export default {
       date: "2022-05-10",
       description: "Even a MacBook needs some care from time to time. Especially if you use your laptop in bed or on your lap, and so on.",
       images: [
-          { name: "image1.jpg", alt: "Apple laptop cleaning kosice" },
-          { name: "image2.jpg", alt: "MacBook cleaning kosice" },
-          { name: "image3.jpg", alt: "overheating MacBook kosice" },
-          { name: "image4.jpg", alt: "overheating Apple kosice" },
+          { name: "image1.jpg", alt: "Apple laptop cleaning" },
+          { name: "image2.jpg", alt: "MacBook cleaning" },
+          { name: "image3.jpg", alt: "overheating MacBook" },
+          { name: "image4.jpg", alt: "overheating Apple" },
       ]
     }
   };
