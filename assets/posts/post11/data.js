9b820b6f75fd79848b8b80dc536978848089c829
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 22:54:46 +0100

    post11 alttxt

--- assets/posts/post11/data.js
+++ assets/posts/post11/data.js
@@ -4,9 +4,9 @@ export default {
       date: "2020-06-10",
       description: "Vyladený BIOS a ovládače, to je už samozrejmosť. Pomoc na diaľku, to je to čo z našeho pohľadu pomôže zákazníkovy najviac, aj dnes sme hneď po zákazníkovom príchode domov pomohli s páru drobnosťami a už si bezstarostne užíva assassin's creed valhalla. ",
       images: [
-          { name: "image1.jpg", alt: "tichy pc kosice" },
-          { name: "image2.jpg", alt: "ticha pc zostava kosice" },
-          { name: "image3.jpg", alt: "noctua kosice" },
+          { name: "image1.jpg", alt: "tichy pc" },
+          { name: "image2.jpg", alt: "ticha pc zostava" },
+          { name: "image3.jpg", alt: "noctua" },
           { name: "image4.jpg", alt: "herna graficka karta" },
           { name: "image5.jpg", alt: "chladenie ventilatormi" },
       ]
@@ -16,10 +16,10 @@ export default {
       date: "2020-06-10",
       description: "Tuned BIOS and drivers are already a given. Remote assistance is what, from our perspective, helps the customer the most. Today, as soon as the customer arrived home, we helped with a couple of minor issues, and now they are enjoying Assassin's Creed Valhalla carefree.",
       images: [
-          { name: "image1.jpg", alt: "silent custom pc kosice" },
-          { name: "image2.jpg", alt: "silent computer kosice" },
-          { name: "image3.jpg", alt: "noctua kosice" },
-          { name: "image4.jpg", alt: "gaming gpu kosice" },
+          { name: "image1.jpg", alt: "silent custom pc" },
+          { name: "image2.jpg", alt: "silent computer" },
+          { name: "image3.jpg", alt: "noctua" },
+          { name: "image4.jpg", alt: "gaming gpu" },
           { name: "image5.jpg", alt: "cooling fans" },
       ]
     }
