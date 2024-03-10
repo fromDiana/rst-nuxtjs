f5600284eabb645c58670ea236e23d4611faa84b
CyanidePower <129548977+CyanidePower@users.noreply.github.com>
2024-03-10 23:00:03 +0100

    post8 alttxt

--- assets/posts/post8/data.js
+++ assets/posts/post8/data.js
@@ -4,10 +4,10 @@ export default {
       date: "2021-08-30",
       description: "Threadripper 3970X od AMD nie je to najjednoduchšie na uchladenie ale s touto perfektnou skrinkou a slušnou nádielkou Noctua fanov sme to zvládli skoro nepočuteľne.",
       images: [
-          { name: "image1.jpg", alt: "Pracovna stanica na mieru kosice" },
-          { name: "image2.jpg", alt: "Pracovna stanica kosice" },
-          { name: "image3.jpg", alt: "Ticha pracovna stanica kosice" },
-          { name: "image4.jpg", alt: "Spolahliva pracovna stanica kosice" },
+          { name: "image1.jpg", alt: "Pracovna stanica na mieru" },
+          { name: "image2.jpg", alt: "Pracovna stanica" },
+          { name: "image3.jpg", alt: "Ticha pracovna stanica" },
+          { name: "image4.jpg", alt: "Spolahliva pracovna stanica" },
       ]
     },
     en: {
@@ -15,10 +15,10 @@ export default {
       date: "2021-08-30",
       description: "The AMD Threadripper 3970X isn't the easiest to cool, but with this perfect case and a decent supply of Noctua fans, we managed to do it almost inaudibly.",
       images: [
-          { name: "image1.jpg", alt: "Custom workstation kosice" },
-          { name: "image2.jpg", alt: "workstation kosice" },
-          { name: "image3.jpg", alt: "Silent workstation kosice" },
-          { name: "image4.jpg", alt: "reliable workstation kosice" },
+          { name: "image1.jpg", alt: "Custom workstation" },
+          { name: "image2.jpg", alt: "workstation" },
+          { name: "image3.jpg", alt: "Silent workstation" },
+          { name: "image4.jpg", alt: "reliable workstation" },
       ]
     }
   };
