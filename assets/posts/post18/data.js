411fc639be5d3747a85f2aaef202f1fed57875c7
Juraj Baksy <jurajbaksy@gmail.com>
2024-06-20 22:52:54 +0200

    Added Post18 and 19

---
+++ assets/posts/post18/data.js
@@ -0,0 +1,27 @@
+export default {
+    sk: {
+      title: "Herný notebook s nefunkčným chladením",
+      date: "2024-06-07",
+      description: "Prepastovanie notebooku, prečistenie od prachu, komplexné čistenie od pasty ktorá je ako náhrada podložiek, návrat do nového stavu.",
+      images: [
+          { name: "image1.jpg", alt: "herný notebook" },
+          { name: "image2.jpg", alt: "čistenie prachu v notebooku" },
+          { name: "image3.jpg", alt: "suchá teplovodivá pasta" },
+          { name: "image4.jpg", alt: "prach v notebooku" },
+          { name: "image5.jpg", alt: "prepastovanie a čistenie notebooku" },
+      ]
+    },
+    en: {
+      title: "Gaming laptop with mal-functing cooling",
+      date: "2024-06-07",
+      description: "Re-pasting the laptop, cleaning it from dust, comprehensive cleaning of the paste used as a substitute for pads, returning it to a like-new condition.",
+      images: [
+          { name: "image1.jpg", alt: "gaming laptop" },
+          { name: "image2.jpg", alt: "cleaning dust from laptop" },
+          { name: "image3.jpg", alt: "dry thermal paste" },
+          { name: "image4.jpg", alt: "dust in a laptop" },
+          { name: "image5.jpg", alt: "repasting and cleaning the laptop" },
+      ]
+    }
+  };
+  
---
+++ assets/posts/post19/data.js
@@ -0,0 +1,27 @@
+export default {
+    sk: {
+      title: "Diagnostika počítača s opravou a prečistením",
+      date: "2024-06-15",
+      description: "Diagnostika poruchy počítača, výmena zdroja, prepastovanie chladenia procesora, komplexné prečistenie od prachu.",
+      images: [
+          { name: "image1.jpg", alt: "špinavý počitač" },
+          { name: "image2.jpg", alt: "prach v chladiči" },
+          { name: "image3.jpg", alt: "zapchaný filter prachu" },
+          { name: "image4.jpg", alt: "prepastovaný a vyčistený chladič" },
+          { name: "image5.jpg", alt: "funkčný a čistý počítač" },
+      ]
+    },
+    en: {
+      title: "Computer diagnostics with repair and cleaning",
+      date: "2024-06-15",
+      description: "Diagnosis of computer malfunction, power supply replacement, re-pasting of CPU cooler, comprehensive dust cleaning.",
+      images: [
+          { name: "image1.jpg", alt: "dirty computer" },
+          { name: "image2.jpg", alt: "dust in the heatsink" },
+          { name: "image3.jpg", alt: "clogged dust filter" },
+          { name: "image4.jpg", alt: "repasted and clean heatsink" },
+          { name: "image5.jpg", alt: "working and clean computer" },
+      ]
+    }
+  };
+  
