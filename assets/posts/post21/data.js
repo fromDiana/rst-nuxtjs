0b0ed322a6d768dc65ec9bd80f9c97148d4dc503
Juraj Baksy <jurajbaksy@gmail.com>
2024-09-22 22:26:16 +0200

    fix ""

--- assets/posts/post21/data.js
+++ assets/posts/post21/data.js
@@ -2,7 +2,7 @@ export default {
     sk: {
       title: "Herný PC Fractal Design: Spoľahlivosť a ochrana pred prachom.",
       date: "2024-07-10",
-      description: "Tento herný PC má skrinku Fractal Design "proti prachu", spoľahlivú grafiku ProArt, chladič Noctua a je postavený pre dlhú životnosť a efektivitu.",
+      description: "Tento herný PC má skrinku Fractal Design proti prachu, spoľahlivú grafiku ProArt, chladič Noctua a je postavený pre dlhú životnosť a efektivitu.",
       images: [
           { name: "image1.jpg", alt: "Skrinka Fractal Design Meshify 2 Compact v čiernej farbe s plným bočným panelom, umiestnená na drevenom stole." },
           { name: "image2.jpg", alt: "Bočný pohľad na otvorenú zostavu v skrinke Fractal Design Meshify 2 Compact, s chladičom Noctua NH-U12S redux a grafickou kartou ASUS ProArt RTX 4070." },
@@ -12,7 +12,7 @@ export default {
     en: {
       title: "Fractal Design Gaming PC: Built for Reliability and Dust Protection.",
       date: "2024-07-10",
-      description: "Featuring a "anti dust" Fractal Design case, ProArt graphics for reliability, and a Noctua cooler for quiet performance, this gaming PC is built for durability and efficiency.",
+      description: "Featuring a anti dust Fractal Design case, ProArt graphics for reliability, and a Noctua cooler for quiet performance, this gaming PC is built for durability and efficiency.",
       images: [
           { name: "image1.jpg", alt: "Fractal Design Meshify 2 Compact case in black, with a solid side panel, placed on a wooden desk." },
           { name: "image2.jpg", alt: "Side view of the open Fractal Design Meshify 2 Compact PC build, featuring a Noctua NH-U12S redux CPU cooler and ASUS ProArt RTX 4070 graphics card." },
